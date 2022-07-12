a2c33369d419e740471f57047972f1250a509f59
Aktir <Margarita.odorodko@gmail.com>
2022-07-12 22:10:17 +0300

    done1

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,6 +1,12 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	//Place your code here
-	return
+	input = [15]float32{1, 2, 3, 4, 5, 6}
+	var sum float32
+	N := 6
+	for i := 0; i <= N; i++ {
+		sum += input[i]
+	}
+	return (sum) / (float32(N))
+
 }
--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,18 @@
 package homework
 
+import "sort"
+
 func sortMapValues(input map[int]string) (result []string) {
 	//Place your code here
+	keyMap := []int{}
+
+	for i := range input {
+		keyMap = append(keyMap, i)
+	}
+	sort.Ints(keyMap)
+
+	for i := 0; i < len(keyMap); i++ {
+		result = append(result, input[keyMap[i]])
+	}
 	return
 }
